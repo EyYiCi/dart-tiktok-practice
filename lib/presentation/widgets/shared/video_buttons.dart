c69f8d373fe1ae9db50f7ed46f373e05b93e3b16
Alejandra GC <dev.ing.agc@outlook.com>
2024-08-25 21:03:02 -0600

    add: Numeros legibles y animate_do

--- lib/presentation/widgets/shared/video_buttons.dart
+++ lib/presentation/widgets/shared/video_buttons.dart
@@ -1,6 +1,8 @@
 
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok/config/domain/entities/video_post.dart';
+import 'package:tiktok/config/helpers/human_formats.dart';
 
 class VideoButtons extends StatelessWidget {
   final VideoPost video;
@@ -12,7 +14,13 @@ class VideoButtons extends StatelessWidget {
     return Column(
       children: [
         _CustomIconButton( value:video.likes, iconColor: Colors.red, iconData: Icons.favorite,),
+        const SizedBox(height: 20),
         _CustomIconButton( value:video.views, iconData: Icons.remove_red_eye_outlined,),
+        const SizedBox(height: 20),
+        SpinPerfect(
+          infinite: true,
+          duration: const Duration(seconds: 5),
+          child: const _CustomIconButton( value:0, iconData: Icons.play_circle_outline,)),
       ],
       
       );
@@ -36,7 +44,8 @@ class _CustomIconButton extends StatelessWidget {
     return Column(children: [
       IconButton(onPressed: (){},
        icon: Icon(iconData, color: color, size: 40)),
-      Text('$value')
+       if(value > 0)
+      Text(HumanFormats.humanReadbleNumber(value.toDouble()))
     ],);
   }
 }
